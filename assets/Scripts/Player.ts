afe80e4cf0bbba9324a41bae52a58ae6065c88bb
guhezhu-guitar <3095677916@qq.com>
2025-09-25 16:25:32 +0800

    新增碰撞事件,小球可以吃掉物体

--- assets/Scripts/Player.ts
+++ assets/Scripts/Player.ts
@@ -1,4 +1,5 @@
-import { _decorator, Component, EventKeyboard, Input, input, KeyCode, Node, RigidBody, Vec2, Vec3 } from 'cc';
+import { _decorator, Collider, Component, EventKeyboard, ICollisionEvent, Input, input, KeyCode, Node, RigidBody, Vec2, Vec3 } from 'cc';
+import { Food } from './Food';
 const { ccclass, property } = _decorator;
 
 @ccclass('Player')
@@ -10,24 +11,65 @@ export class Player extends Component {
     @property
     public moveForce = 5;
 
+    //定义碰撞器组件并得到碰撞器
+    private collider : Collider = null;
+
     //定义移动的方向,x代表左右,y代表上下
      private moveDir:Vec2 = Vec2.ZERO;
      //获取rgd(定义的节点)上的RigBody组件
      private rgd:RigidBody =null;
+
      protected start(): void {
          this.rgd = this.getComponent(RigidBody);
+
+        this.collider =this.node.getComponent(Collider);
+
+        this.collider.on('onCollisionEnter',this.onCollisionEnter,this);
+        this.collider.on('onCollisionExit',this.onCollisionExit,this);
+        this.collider.on('onCollisionStay',this.onCollisionStay,this);
+
      }
 
     protected onLoad(): void {
+        //取得碰撞器组件
+        //不在onlord里面触发,换到start方法里面
+        // this.collider =this.node.getComponent(Collider);
+
+        // this.collider.on('onCollisionEnter',this.onCollisionEnter,this);
+        // this.collider.on('onCollisionExit',this.onCollisionExit,this);
+        // this.collider.on('onCollisionStay',this.onCollisionStay,this);
+
         input.on(Input.EventType.KEY_DOWN,this.onkeyDown,this);
         input.on(Input.EventType.KEY_PRESSING,this.onkeyPressing,this);
         input.on(Input.EventType.KEY_UP,this.onkeyUp,this);
     }
+    //监听碰撞事件
+    onCollisionEnter(event:ICollisionEvent){
+        const food = event.otherCollider.getComponent(Food);
+        //检测小球是否和food发生碰撞,如果发生碰撞,则销毁food节点
+        if(food!=null){
+            food.node.destroy();
+        }
+        // console.log('onCollisionEnter');
+    }
+
+    onCollisionExit(){
+        // console.log('onCollisionExit');
+    }
+
+    onCollisionStay(){
+        // console.log('onCollisionStay');
+    }
+
 
     protected onDestroy(): void {
         input.off(Input.EventType.KEY_DOWN,this.onkeyDown,this);
         input.off(Input.EventType.KEY_PRESSING,this.onkeyPressing,this);
         input.off(Input.EventType.KEY_UP,this.onkeyUp,this);
+
+        this.collider.off('onCollisionEnter',this.onCollisionEnter,this);
+        this.collider.off('onCollisionExit',this.onCollisionExit,this);
+        this.collider.off('onCollisionStay',this.onCollisionStay,this);
     }
     
     //AD控制的是z轴的分量,WS控制的是x轴的分量
@@ -52,19 +94,18 @@ export class Player extends Component {
 
     onkeyUp(event:EventKeyboard){
         // console.log('onkeyUp'+event.keyCode);
-        switch(event.keyCode){
+            switch(event.keyCode){
             case KeyCode.KEY_A:
-            this.moveDir.x =0;
+            this.moveDir =new Vec2(0,this.moveDir.y);
             break;
             case KeyCode.KEY_D:
-            this.moveDir.x =0;
+            this.moveDir = new Vec2(0,this.moveDir.y);
             break;
             case KeyCode.KEY_W:
-            this.moveDir.y =0;
+            this.moveDir =new Vec2(this.moveDir.x,0);
             break;
             case KeyCode.KEY_S:
-            this.moveDir.y =0;
-            break;
+            this.moveDir =new Vec2(this.moveDir.x,0);
         }
 
     }
